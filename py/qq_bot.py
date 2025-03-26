355cfe56dceba8e8a5b94051ed8928df59a7a292
AntidOTide <1158519478@qq.com>
2025-03-26 17:04:03 +0800

    新增了jm工具

--- py/qq_bot.py
+++ py/qq_bot.py
@@ -4,6 +4,7 @@ from loguru import logger
 from send_message import SendMessage
 
 
+# /upload_group_file
 class QQBot(SendMessage):
     @staticmethod
     def send_private_message(url: str, uid: int, message: str):
@@ -11,6 +12,28 @@ class QQBot(SendMessage):
             logger.info(f'发送到用户<{uid}>的消息是：{message}')
             res = requests.post(url=url + "/send_private_msg",
                                 params={'user_id': uid, 'message': message}).json()
+            if res["status"] == "ok":
+                logger.info("私聊消息发送成功")
+                return "私聊消息发送成功"
+            else:
+                logger.info(res)
+                logger.info("私聊消息发送失败，错误信息：" + str(res['wording']))
+                return "私聊消息发送失败，错误信息：" + str(res['wording'])
+        except Exception as error:
+            logger.error("私聊消息发送失败")
+            logger.error(error)
+            return "私聊消息发送失败"
+
+    @staticmethod
+    def send_group_file(url: str, gid: int, file_path: str, name: str):
+        try:
+            logger.info(f'发送到用户<{gid}>的消息是')
+            res = requests.post(url=url + "/upload_group_file",
+                                params={
+                                    'group_id': gid,
+                                    'file': file_path,
+                                    'name': name
+                                }).json()
             if res["status"] == "ok":
                 logger.info("私聊消息发送成功")
                 return "私聊消息发送成功"
